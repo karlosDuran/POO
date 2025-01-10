eeb589a1223ba273ec788324d95cbd9075da88c6
karlosDuran <ferni0425@gmail.com>
2025-01-10 13:31:03 -0600

    creando el metodo atacar

--- poo_2c.py
+++ poo_2c.py
@@ -1,22 +1,67 @@
 class personaje:
     #atributos de la clase
-    nombre="Default"
-    fuerza=0
-    inteligencia=0
-    defensa=0
-    vida=0
+    #nombre="Default"
+    #fuerza=0
+    #inteligencia=0
+    #defensa=0
+    #vida=0
+    def __init__(self, nombre, fuerza, inteligencia, defensa, vida):
+        self.nombre=nombre
+        self.fuerza=fuerza
+        self.inteligencia=inteligencia
+        self.defensa=defensa
+        self.vida=vida
+    #¿Qué es self? es una referencia al mismo objeto
+    #que el el metodo init? constructor que inicializa los atributos de un objeto
+    #pq se usa doble guion bajo? Dunder. porque es un metodo magico.
+    #cuando se ejecuta el metodo init? Autom. al crear una nueva instacia u objeto
+    def imprimir_atributos(self):
+        print(self.nombre)
+        print("-fuerza:",self.fuerza)
+        print("-inteligencia:",self.inteligencia)
+        print("-defensa:",self.defensa)
+        print("-vida:",self.vida)
+    
+    def subir_nivel(self,fuerza,inteligencia,defensa):
+        self.fuerza += fuerza
+        self.inteligencia += inteligencia
+        self.defensa += defensa
 
-#variable del constructor vacio 
-mi_personaje= personaje()
+    def está_vivo(self):
+        return self.vida > 0
+
+    def morir(self):
+        self.vida=0
+        print(self.nombre,"ha muerto")
+        
+    def dañar(self,enemigo):
+        return self.fuerza - enemigo.defensa 
+    
+    def atacar(self,enemigo):
+        daño = self.dañar(enemigo)
+        enemigo.vida = enemigo.vida- daño
+        print(self.nombre,"Ha hecho",daño,"puntos de daño a",enemigo.nombre)
+        print("Vida de", enemigo.nombre, "es", enemigo.vida)
+
+#variable del constructor 
+mi_personaje= personaje("master chif",100,50,45,100)
+mi_personaje.imprimir_atributos()
+mi_enemigo= personaje("El malo",10,50,45,100)
+mi_personaje.atacar(mi_enemigo)
+mi_enemigo.imprimir_atributos()
+
+# mi_personaje.subir_nivel(15,20,30)
+# print("Valores actualizados")
+# mi_personaje.imprimir_atributos()
 #modificando valores de los atributos
-mi_personaje.nombre="jhon 117"
-mi_personaje.fuerza=200000
-mi_personaje.inteligencia=44
-mi_personaje.defensa=44
-mi_personaje.vida=1
+#mi_personaje.nombre="jhon 117"
+#mi_personaje.fuerza=200000
+#mi_personaje.inteligencia=44
+#mi_personaje.defensa=44
+#mi_personaje.vida=1
 
-print("El nombre de mi personaje es: ",mi_personaje.nombre)
-print("La fuerza mi personaje es: ",mi_personaje.fuerza)
-print("La inteligencia de mi personaje es: ",mi_personaje.inteligencia)
-print("La defensa de mi personaje es: ",mi_personaje.defensa)
-print("La vida de mi personaje es: ",mi_personaje.vida)
+#print("El nombre de mi personaje es: ",mi_personaje.nombre)
+#print("La fuerza mi personaje es: ",mi_personaje.fuerza)
+#print("La inteligencia de mi personaje es: ",mi_personaje.inteligencia)
+#print("La defensa de mi personaje es: ",mi_personaje.defensa)
+#print("La vida de mi personaje es: ",mi_personaje.vida)
